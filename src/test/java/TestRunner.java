de45b9451f680a298aaf14084f7fb15efb9d15e8
ishwarya2027 <79291554+ishwarya2027@users.noreply.github.com>
2021-02-19 15:20:11 +0000

    Update TestRunner.java

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,9 +1,15 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = { "src/test/resource/featureFile" }, glue = {
 		"stepDefinition" }, tags = "@2020feature", plugin = { "pretty", "html:target/HtmlReports" })
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 
-
+	@Override
+	@DataProvider(parallel = false)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
 }
